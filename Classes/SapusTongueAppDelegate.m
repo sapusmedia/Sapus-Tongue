0ae7c9412be0a05a2511022c2cbfaa31c0eca1ca
Ricardo Quesada <ricardoquesada@gmail.com>
2013-01-28 14:49:41 -0800

    Uses cocos2d v2.1-rc0

--- Classes/SapusTongueAppDelegate.m
+++ Classes/SapusTongueAppDelegate.m
@@ -50,8 +50,8 @@
 // The available orientations should be defined in the Info.plist file.
 // And in iOS 6+ only, you can override it in the Root View controller in the "supportedInterfaceOrientations" method.
 // Only valid for iOS 6+. NOT VALID for iOS 4 / 5.
--(NSUInteger)supportedInterfaceOrientations {
-
+-(NSUInteger)supportedInterfaceOrientations
+{
 	// iPhone only
 	if( [[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone )
 		return UIInterfaceOrientationMaskLandscape;
